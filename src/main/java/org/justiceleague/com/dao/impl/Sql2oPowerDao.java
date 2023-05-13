d5b70300725f8b774e3bed6a64877e74913db13e
Lawrence Karani <lawrencekarani@gmail.com>
2023-05-13 21:22:43 +0300

    created Sql2oPowerDao

---
+++ src/main/java/org/justiceleague/com/dao/impl/Sql2oPowerDao.java
@@ -0,0 +1,38 @@
+package org.justiceleague.com.dao.impl;
+
+import org.justiceleague.com.dao.powersDao;
+import org.justiceleague.com.models.Power;
+
+import java.util.List;
+
+public class Sql2oPowerDao implements powersDao {
+    @Override
+    public List<Power> getAll() {
+        return null;
+    }
+
+    @Override
+    public void add(Power power) {
+
+    }
+
+    @Override
+    public Power findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String description) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAllPowers() {
+
+    }
+}
