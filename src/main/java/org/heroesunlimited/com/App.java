647166928cce11cdaacd89dc78953b454e41d5f8
Lawrence Karani <lawrencekarani@gmail.com>
2023-06-01 14:56:30 +0300

    added static file location

--- src/main/java/org/heroesunlimited/com/App.java
+++ src/main/java/org/heroesunlimited/com/App.java
@@ -1,8 +1,38 @@
 package org.heroesunlimited.com;
 
+import static spark.Spark.staticFileLocation;
+
 public class App {
 
     public static void main(String... args) {
+
+        staticFileLocation("/public");
+
+        // show home page
+
+
+        // show hero hero
+
+        // add/edit hero
+
+        // delete hero
+
+
+        // show squad
+
+        // add/edit squad
+
+
+        // delete squad
+
+
+        // show powers
+
+        //add /edit powers
+
+        // show weaknesses
+
+        // add/edit weaknesses
 
     }
 }
