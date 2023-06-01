b1d54c6c8297eb58ca7ea60a0ec473b99a6aee74
Lawrence Karani <lawrencekarani@gmail.com>
2023-06-01 15:32:55 +0300

    added dbConfig and spark methods in App.java

--- src/main/java/org/heroesunlimited/com/App.java
+++ src/main/java/org/heroesunlimited/com/App.java
@@ -1,6 +1,26 @@
 package org.heroesunlimited.com;
 
-import static spark.Spark.staticFileLocation;
+import org.heroesunlimited.com.config.DbConfig;
+import org.heroesunlimited.com.dao.HerosDao;
+import org.heroesunlimited.com.dao.PowersDao;
+import org.heroesunlimited.com.dao.SquadDao;
+import org.heroesunlimited.com.dao.WeaknessesDao;
+import org.heroesunlimited.com.dao.impl.Sql2oHeroDao;
+import org.heroesunlimited.com.dao.impl.Sql2oPowerDao;
+import org.heroesunlimited.com.dao.impl.Sql2oSquadDao;
+import org.heroesunlimited.com.dao.impl.Sql2oWeaknessDao;
+import org.heroesunlimited.com.models.Hero;
+import org.heroesunlimited.com.models.Power;
+import org.heroesunlimited.com.models.Squad;
+import org.heroesunlimited.com.models.Weakness;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class App {
 
@@ -8,31 +28,194 @@ public class App {
 
         staticFileLocation("/public");
 
+        // Initialize DAOs
+        HerosDao herosDao = new Sql2oHeroDao(DbConfig.datasource());
+        SquadDao squadDao = new Sql2oSquadDao(DbConfig.datasource());
+        PowersDao powersDao = new Sql2oPowerDao(DbConfig.datasource());
+        WeaknessesDao weaknessesDao = new Sql2oWeaknessDao(DbConfig.datasource());
+
+
         // show home page
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "index.hbs"));
+        });
+
+        // show hero list
+        get("/heroes", (req, res) -> {
+            List<Hero> heroes = herosDao.getAll();
+            Map<String, Object> model = new HashMap<>();
+            model.put("heroes", heroes);
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "heroes.hbs"));
+        });
+
+        // get add hero form
+        get("/heroes/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form (e.g., squads, powers, weaknesses)
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "heroForm.hbs"));
+        });
+
+        // add hero hero
+        post("/heroes/new", (req, res) -> {
+            // Code to handle hero form submission
+            res.redirect("/heroes");
+            return null;
+        });
+
+        // get update hero form
+        get("/heroes/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form (e.g., squads, powers, weaknesses)
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "heroForm.hbs"));
+        });
+
+        // add hero hero
+        post("/heroes/:id", (req, res) -> {
+            // Code to handle hero form submission
+            res.redirect("/heroes");
+            return null;
+        });
 
 
-        // show hero hero
+        // delete a hero
+        get("/heroes/:id/delete", (req, res) -> {
+            int heroId = Integer.parseInt(req.params("id"));
+            herosDao.deleteById(heroId);
+            res.redirect("/heroes");
+            return null;
+        });
 
-        // add/edit hero
+        // show squad list
+        get("/squads", (req, res) -> {
+            List<Squad> squads = squadDao.getAll();
+            Map<String, Object> model = new HashMap<>();
+            model.put("squads", squads);
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "squads.hbs"));
+        });
 
-        // delete hero
+        // show add squad form
+        get("/squads/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "squadForm.hbs"));
+        });
+        // add squad
+        post("/squads/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "squadForm.hbs"));
+        });
 
+        get("/squads/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "squadForm.hbs"));
+        });
 
-        // show squad
+        // update squad
+        post("/squads/:id", (req, res) -> {
+            // Code to handle squad form submission
+            res.redirect("/squads");
+            return null;
+        });
 
-        // add/edit squad
+        // delete a squad
+        get("/squads/:id/delete", (req, res) -> {
+            int squadId = Integer.parseInt(req.params("id"));
+            squadDao.deleteById(squadId);
+            res.redirect("/squads");
+            return null;
+        });
 
+        // show powers list
+        get("/powers", (req, res) -> {
+            List<Power> powers = powersDao.getAll();
+            Map<String, Object> model = new HashMap<>();
+            model.put("powers", powers);
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "powers.hbs"));
+        });
 
-        // delete squad
+        // show new power form
+        get("/powers/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "powerForm.hbs"));
+        });
 
+        // add  a power
+        post("/powers/new", (req, res) -> {
+            // Code to handle power form submission
+            res.redirect("/powers");
+            return null;
+        });
 
-        // show powers
+        // show update power form
+        get("/powers/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "powerForm.hbs"));
+        });
 
-        //add /edit powers
+        // update a power
+        post("/powers/:id", (req, res) -> {
+            // Code to handle power form submission
+            res.redirect("/powers");
+            return null;
+        });
 
-        // show weaknesses
+        // delete a power
+        get("/powers/:id/delete", (req, res) -> {
+            int powerId = Integer.parseInt(req.params("id"));
+            powersDao.deleteById(powerId);
+            res.redirect("/powers");
+            return null;
+        });
 
-        // add/edit weaknesses
+        // show weaknesses list
+        get("/weaknesses", (req, res) -> {
+            List<Weakness> weaknesses = weaknessesDao.getAll();
+            Map<String, Object> model = new HashMap<>();
+            model.put("weaknesses", weaknesses);
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "weaknesses.hbs"));
+        });
+
+        // show add weakness form
+        get("/weaknesses/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "weaknessForm.hbs"));
+        });
+
+        // add a weakness
+        post("/weaknesses/new", (req, res) -> {
+            // Code to handle weakness form submission
+            res.redirect("/weaknesses");
+            return null;
+        });
+
+        // show update weakness form
+        get("/weaknesses/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            // Code to retrieve necessary data for the form
+            return new HandlebarsTemplateEngine().render(new ModelAndView(model, "weaknessForm.hbs"));
+        });
+
+        // update a weakness
+        post("/weaknesses/:id", (req, res) -> {
+            // Code to handle weakness form submission
+            res.redirect("/weaknesses");
+            return null;
+        });
+
+        // delete a power
+        get("/weaknesses/:id/delete", (req, res) -> {
+            int weaknessId = Integer.parseInt(req.params("id"));
+            weaknessesDao.deleteById(weaknessId);
+            res.redirect("/weaknesses");
+            return null;
+        });
+
 
     }
 }
---
+++ src/main/java/org/heroesunlimited/com/config/DbConfig.java
@@ -0,0 +1,30 @@
+package org.heroesunlimited.com.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class DbConfig {
+    //connect to psql datasource
+    public static Sql2o datasource (){
+        Dotenv dotenv = Dotenv.load();
+        String url = dotenv.get("dbUrl") != null ? dotenv.get("dbUrl") : System.getenv("dbUrl");
+        String user = dotenv.get("dbUsername") != null ? dotenv.get("dbUsername") : System.getenv("dbUsername");
+        String password = dotenv.get("dbPassword") != null ? dotenv.get("dbPassword") : System.getenv("dbPassword");
+        Sql2o sql2o = new Sql2o(url, user, password);
+
+        // Get the SQL script
+        InputStream inputStream = DbConfig.class.getClassLoader().getResourceAsStream("db/createdb.sql");
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+        String sqlScript = scanner.hasNext() ? scanner.next() : "";
+        // Execute the SQL script to set up database
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery(sqlScript).executeUpdate();
+        }
+
+        return sql2o;
+    }
+}
